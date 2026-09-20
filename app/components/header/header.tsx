3547092dca0fee9b9a0ed919ea58c0cf72d72312
JakeVaio <jake.gregory.vaio@gmail.com>
2026-09-20 21:21:19 +0100

    Project setup

---
+++ app/components/header/header.tsx
@@ -0,0 +1,9 @@
+export function Header() {
+  return (
+    <header>
+      <div>
+        <p>Header</p>
+      </div>
+    </header>
+  );
+}
---
+++ app/components/sections/header-info-image-two-column.tsx
@@ -0,0 +1,3 @@
+export function HeaderInfoImageTwoColumn() {
+  return <div>HeaderInfoImageTwoColumn</div>;
+}
